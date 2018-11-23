8e2914792aa84922647a4151f0012a05c5a093dc
Daniel Irvine <daniel@danielirvine.com>
2018-11-23 19:05:45 +0000

    Solution part one

--- src/bank_ocr_kata/ocr.cljs
+++ src/bank_ocr_kata/ocr.cljs
@@ -1,4 +1,58 @@
 (ns bank-ocr-kata.ocr
   (:require [clojure.string]))
 
-(defn read-single-character [s])
+(def characters
+  [(str " _ "
+        "| |"
+        "|_|")
+
+   (str "   "
+        "  |"
+        "  |")
+
+   (str " _ "
+        " _|"
+        "|_ ")
+
+   (str " _ "
+        " _|"
+        " _|")
+
+   (str "   "
+        "|_|"
+        "  |")
+
+   (str " _ "
+        "|_ "
+        " _|")
+
+   (str " _ "
+        "|_ "
+        "|_|")
+
+   (str " _ "
+        "  |"
+        "  |")
+
+   (str " _ "
+        "|_|"
+        "|_|")
+
+   (str " _ "
+        "|_|"
+        " _|")
+   ])
+
+(defn read-single-character [s]
+  (.indexOf characters s))
+
+(defn partition-strs [n s]
+  (map #(apply str %) (partition n s)))
+
+(defn read-many [s]
+  (let [[top middle bottom] (clojure.string/split s #"\n")
+        top-chs (partition-strs 3 top)
+        middle-chs (partition-strs 3 middle)
+        bottom-chs (partition-strs 3 bottom)
+        character-strings (map #(str %1 %2 %3) top-chs middle-chs bottom-chs)]
+    (apply str (map read-single-character character-strings))))
--- test/bank_ocr_kata/ocr_test.cljs
+++ test/bank_ocr_kata/ocr_test.cljs
@@ -3,5 +3,33 @@
             [bank-ocr-kata.ocr :as ocr]))
 
 (deftest read-single-character
+  (testing "reads a 0"
+    (is (= 0 (ocr/read-single-character (nth ocr/characters 0)))))
   (testing "reads a 1"
-    (is (= 1 (ocr/read-single-character "???")))))
+    (is (= 1 (ocr/read-single-character (nth ocr/characters 1)))))
+  (testing "reads a 2"
+    (is (= 2 (ocr/read-single-character (nth ocr/characters 2)))))
+  (testing "reads a 3"
+    (is (= 3 (ocr/read-single-character (nth ocr/characters 3)))))
+  (testing "reads a 4"
+    (is (= 4 (ocr/read-single-character (nth ocr/characters 4)))))
+  (testing "reads a 5"
+    (is (= 5 (ocr/read-single-character (nth ocr/characters 5)))))
+  (testing "reads a 6"
+    (is (= 6 (ocr/read-single-character (nth ocr/characters 6)))))
+  (testing "reads a 7"
+    (is (= 7 (ocr/read-single-character (nth ocr/characters 7)))))
+  (testing "reads a 8"
+    (is (= 8 (ocr/read-single-character (nth ocr/characters 8)))))
+  (testing "reads a 9"
+    (is (= 9 (ocr/read-single-character (nth ocr/characters 9))))))
+
+(deftest read-many-characters
+  (testing "reads 10"
+    (is (= "10" (ocr/read-many (str "    _ \n"
+                                    "  || |\n"
+                                    "  ||_|\n")))))
+  (testing "reads 012345678"
+    (is (= "012345678" (ocr/read-many (str " _     _  _     _  _  _  _ \n"
+                                           "| |  | _| _||_||_ |_   ||_|\n"
+                                           "|_|  ||_  _|  | _||_|  ||_|\n"))))))
